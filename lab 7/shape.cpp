4a4e7900490958b43269f6331a682ba7f7cb572b
swornim-59 <swornimpandey5960@gmail.com>
2026-07-07 22:09:16 +0545

    chore: relocate code to swornim-pandey-kec repository stack

--- lab 7/shape.cpp
+++
@@ -1,2 +0,0 @@
-// class shape derive class circle rectangle and triangle and show the use of virtual function in this program and in main make a shape poinnter and point to
-// each derived class object and call the virtual function using base class pointeralso make pointer a array to store 3 data of 3 devived class and cll using for loop to print thee data
